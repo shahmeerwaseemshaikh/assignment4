7816ebbd342064d586757e1e801375b73a8f1f1b
Shahmeer Shaikh <65039014+shahmeerwaseemshaikh@users.noreply.github.com>
2021-12-02 02:20:12 -0800

    Add files via upload

---
+++ assignment4.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+
+void main() {
+  // var a={"dell","hp","laptop","lcd"};
+  // print(a);
+
+// var b={1,2,"dell",true,false};
+// print(b);
+
+// List c=["SSC","HSC","BCS","BS","BCOM","MS","M.Phill","Phd"];
+// print("${c[0]},${c[1]},${c[2]},${c[3]},${c[4]},${c[5]},${c[6]},${c[7]}");
+
+// int total=500;
+// List student=["shahmeer","azmat","samad"];
+// List marks=[330,320,400];
+// print("student 1 : ${student[0]} Obt marks : ${marks[0]} and per : ${marks[0]/total*100}");
+// print("student 2 : ${student[1]} Obt marks : ${marks[1]} and per : ${marks[1]/total*100}");
+// print("student 3 : ${student[2]} Obt marks : ${marks[2]} and per : ${marks[2]/total*100}");
+
+// List arr=[330,320,400];
+// arr.sort();
+// print(arr);
+
+// List arr1=[330,320,400];
+// print(arr1.reversed);
+
+// List arr3=["Python","MySQL","C","C++","ES6","Java"];
+// if (arr3[2] !=null) {
+//   arr3.replaceRange(2, 3, ["Dart"]);
+//   print(arr3);
+  
+}
+
